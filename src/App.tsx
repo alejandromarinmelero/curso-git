a10ff6a38d2589eb68411ea195456bcb1049be7e
Alex <alex_clasiko@hotmail.es>
2022-12-13 22:10:59 +0100

    componente NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import NavBar from './componentes/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
     </div>
   );
 }
